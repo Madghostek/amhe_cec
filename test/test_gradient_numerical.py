working f1-f4 gradient, fixed stochastic randomness in gradients

test/test_gradient_numerical.py
@@ -1,10 +1,14 @@
 import numpy as np
 from amhe_cec.benchmark import BenchmarkFactory
+from amhe_cec._amhe_cec_impl import cec2005_disablerand, cec2005_enablerand
 import pytest
 
+
+# year, function, dimensions
 test_data = [("cec2005",1,3),
              ("cec2005",2,3),
-             ("cec2005",3,2)]
+             ("cec2005",3,2),
+             ("cec2005",4,3)]
 
 def numerical_gradient(f, x, eps=1e-6):
     grad = np.zeros_like(x)
@@ -24,6 +28,11 @@ def test_gradient_parametrised(target_bench,target_func, dim):
     x = np.random.random(dim)
 
     b = BenchmarkFactory.get(target_bench, target_func, dim)
+ 
+    cec2005_disablerand() # disable randomness for numerical evaluation (one sample is stored)
+    # don't remove this, otherwise randomness won't be initialized
+    b.evaluate(x) # call the function first, in case there is randomness (2005 f4)
+
 
     true_grad = b.gradient(x)
     num_grad = numerical_gradient(b.evaluate,x)
@@ -32,4 +41,6 @@ def test_gradient_parametrised(target_bench,target_func, dim):
     print("Difference (L2 norm):", diff)
 
     rel_error = diff / (np.linalg.norm(true_grad) + np.linalg.norm(num_grad))
-    assert rel_error < 1e-6, "Gradient check failed!"+target_bench+str(target_func)
+    assert rel_error < 1e-6, "Gradient check failed!"+target_bench+", "+str(target_func)+", "+str(num_grad)+" "+str(true_grad)
+
+    cec2005_enablerand() # restore